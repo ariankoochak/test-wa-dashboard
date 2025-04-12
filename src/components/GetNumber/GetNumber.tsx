e12edc882fc7b002f1f3b4e349c24903f31e0fa9
Arian <arian.koochak@protonmail.com>
2025-04-12 19:00:12 +0330

    complete getNumber component logic

--- src/components/GetNumber/GetNumber.tsx
+++ src/components/GetNumber/GetNumber.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setRandomNumber } from "../../store/myRandomNumberSlice";
+
+import {isNumeric} from 'valiend';
 
 const GetNumber: React.FC = () => {
-    return <>تست میکنیم</>;
+    const [count, setCount] = useState<number>(0); 
+    const dispatch = useDispatch();
+
+    const handleChangeCount = (e : any)=>{
+        const newValue = e.target.value;
+        if (isNumeric(newValue)) 
+            setCount(e.target.value);
+    }
+
+    const handleClickSubmit = ()=>{
+        dispatch(setRandomNumber(count));
+    }
+
+    return <div className="input-container">
+        <input type="text" value={count} onChange={handleChangeCount}/>
+        <button onClick={handleClickSubmit}>Submit</button>
+    </div>;
 };
 
 export default GetNumber;
