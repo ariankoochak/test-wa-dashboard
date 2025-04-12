8fccc524b5b33b1409183c3e874ada4aa60e42a6
Arian <arian.koochak@protonmail.com>
2025-04-12 18:37:06 +0330

    add components & redux provider

--- src/TestWaDashboard.tsx
+++ src/TestWaDashboard.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import Chart from "./components/Chart/Chart";
+import GetNumber from "./components/GetNumber/GetNumber";
 
-type Props = {
-    myRandomNumber: number;
-};
-
-const MyComponent: React.FC<Props> = ({myRandomNumber}) => {
-    return <>{myRandomNumber}</>;
+const MyComponent: React.FC = () => {
+    return <>
+    <Provider store={store}>
+        <GetNumber/>
+        <Chart/>
+    </Provider>
+    </>;
 };
 
 export default MyComponent;
