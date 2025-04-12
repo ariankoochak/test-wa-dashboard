45552666ffc02c2d74ab180aa4465699c0a01e6b
Arian <arian.koochak@protonmail.com>
2025-04-12 19:05:09 +0330

    add static data & chart primary structure

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,14 +1,119 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
 const Chart: React.FC = () => {
-    const dispatch = useDispatch();
     const number = useSelector((state: RootState) => state.my.myRandomNumber);
-    
-    return <>
-    <div>{number}</div>
-    </>
+
+    const options: Highcharts.Options = {
+        chart: {
+            type: "column",
+        },
+        title: {
+            text: "World's largest cities per 2021",
+        },
+        subtitle: {
+            text: 'Source: <a href="https://worldpopulationreview.com/world-cities" target="_blank">World Population Review</a>',
+        },
+        xAxis: {
+            type: "category",
+            labels: {
+                autoRotation: [-45, -90],
+                style: {
+                    fontSize: "13px",
+                    fontFamily: "Verdana, sans-serif",
+                },
+            },
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: "Population (millions)",
+            },
+        },
+        legend: {
+            enabled: false,
+        },
+        tooltip: {
+            pointFormat: "Population in 2021: <b>{point.y:.1f} millions</b>",
+        },
+        series: [
+            {
+                name: "Population",
+                type: "column",
+                colorByPoint: true,
+                groupPadding: 0,
+                colors: [
+                    "#9b20d9",
+                    "#9215ac",
+                    "#861ec9",
+                    "#7a17e6",
+                    "#7010f9",
+                    "#691af3",
+                    "#6225ed",
+                    "#5b30e7",
+                    "#533be1",
+                    "#4c46db",
+                    "#4551d5",
+                    "#3e5ccf",
+                    "#3667c9",
+                    "#2f72c3",
+                    "#277dbd",
+                    "#1f88b7",
+                    "#1693b1",
+                    "#0a9eaa",
+                    "#03c69b",
+                    "#00f194",
+                ],
+                data: [
+                    ["Tokyo", 37.33],
+                    ["Delhi", 31.18],
+                    ["Shanghai", 27.79],
+                    ["Sao Paulo", 22.23],
+                    ["Mexico City", 21.91],
+                    ["Dhaka", 21.74],
+                    ["Cairo", 21.32],
+                    ["Beijing", 20.89],
+                    ["Mumbai", 20.67],
+                    ["Osaka", 19.11],
+                    ["Karachi", 16.45],
+                    ["Chongqing", 16.38],
+                    ["Istanbul", 15.41],
+                    ["Buenos Aires", 15.25],
+                    ["Kolkata", 14.974],
+                    ["Kinshasa", 14.97],
+                    ["Lagos", 14.86],
+                    ["Manila", 14.16],
+                    ["Tianjin", 13.79],
+                    ["Guangzhou", 13.64],
+                ],
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: "#FFFFFF",
+                    inside: true,
+                    verticalAlign: "top",
+                    format: "{point.y:.1f}",
+                    y: 10,
+                    style: {
+                        fontSize: "13px",
+                        fontFamily: "Verdana, sans-serif",
+                    },
+                },
+            },
+        ],
+    };
+
+    return (
+        <>
+            <div className="chart-container">
+                <HighchartsReact highcharts={Highcharts} options={options} />
+            </div>
+        </>
+    );
 };
 
 export default Chart;
